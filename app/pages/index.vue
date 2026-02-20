90b9187bfc491cbb181cf2bfd954e4db3976ba65
Marko Peedosk <markopeedosk@gmail.com>
2026-02-20 17:34:50 +0200

    Feature: simple list of localities (first page only)

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {useLocalities} from "~/composable/useLocalities";
+
+const data = await useLocalities();
+</script>
+
+<template>
+  <li v-for="item in data.results">
+    <NuxtLink :to="{ name: 'localities-id', params: { id: item.id } }">
+      <h1>{{item.id}}, {{item.name}}, {{item.country}}</h1>
+    </NuxtLink>
+  </li>
+</template>
