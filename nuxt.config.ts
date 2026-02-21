8ba858f7b20534b7b66b91535ae1d868d1aca86b
Marko Peedosk <markopeedosk@gmail.com>
2026-02-21 19:36:19 +0200

    Fix: netlify config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: [
-      "@nuxt/ui",
-      "@vueuse/nuxt",
-  ],
-  css: ['~/assets/css/main.css']
+    future: {
+        compatibilityVersion: 4,
+    },
+
+    compatibilityDate: '2025-07-15',
+
+
+    nitro: {
+        preset: 'netlify'
+    },
+
+    devtools: { enabled: true },
+
+    modules: [
+        "@nuxt/ui",
+        "@vueuse/nuxt",
+    ],
+
+    css: ['~/assets/css/main.css'],
+
+    ssr: true
 })
